6ab1b9ed3680d662f551816ffbe62e3d9e5c4488
Miguel Ben <itsmigben@gmail.com>
2019-12-30 05:31:19 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,128 @@
 # My Code here....
+def map_to_negativize(arr)
+  
+  result = []
+  
+  arr.each {|ele|
+    result.push(ele * -1)
+  }
+  
+  return result
+  
+end
+
+
+def map_to_no_change(arr)
+  
+  result = []
+  
+  arr.each {|ele|
+    result.push(ele)
+    
+  }
+  
+  return result
+  
+end
+
+
+def map_to_double(arr)
+  
+  result = []
+  
+  arr.each{|ele|
+    result.push(ele * 2)
+    
+  }
+  return result
+  
+end
+
+
+def map_to_square(arr)
+  
+  result = []
+  
+  arr.each {|ele|
+    result.push(ele ** 2)
+  }
+  
+  return result
+  
+end
+
+
+def reduce_to_total(arr, start = 0 ) # default val int to zero if no arg is provided.
+  
+  result = start
+  
+  arr.each {|ele|
+    result += ele
+  }
+  
+  return result
+  
+end
+
+
+def reduce_to_all_true(arr)
+  
+  arr.each {|ele|
+    if ele == false
+      return false
+
+    end
+  }
+  
+  return true
+    
+end
+
+
+def reduce_to_any_true(arr)
+  
+  arr.each {|ele|
+    if ele
+      return true
+    end
+    
+  }
+  
+  return false
+  
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
